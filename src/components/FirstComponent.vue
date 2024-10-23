3451895f5322133c5c77f8974ff7538d6280ea66
Salem Bahri <bahrisalem5@gmail.com>
2024-10-23 02:21:41 +0100

    create methods

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -5,6 +5,8 @@
     <p>my age is : {{age}}</p>
     <p>my country is : {{country[0]}}</p>
     <p>I am maried : {{verif}}</p>
+    <p>{{getMessage()}}</p>
+    <p>{{getAge()}}</p>
     
 </div>
 </template>
@@ -21,6 +23,15 @@ export default {
             country:['tunisia','algerie','egypt','jordan'],
             verif:true
         }
+    },
+    methods:{
+        getMessage(){
+            return 'hello from function'
+        },
+        getAge(){
+            return 'my age is 50'
+        }
+
     }
 
 }
